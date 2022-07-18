335209d424dca93ce4ae53bab39c4caefeaea622
trofi92 <84131398+trofi92@users.noreply.github.com>
2022-07-18 18:03:37 +0900

    commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function loginPopUp (){
+    const url = "file:///home/edu02/sample/login.html";
+    const name = "LOG IN";
+    const option = "width=500px, height=500px, top=100px, left=200px, location = no"; 
+
+    window.open(url, name, option);
+};
